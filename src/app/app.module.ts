d88922a506192c5ed54640e427131b90c7ff21ef
Vasilis Milionis <v_milionis@yahoo.com>
2021-09-02 11:18:49 +0300

    Generates dropdown and dropdown-item components using AngularCLI

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DropdownComponent } from './dropdown/dropdown.component';
+import { DropdownItemComponent } from './dropdown/dropdown-item/dropdown-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DropdownComponent,
+    DropdownItemComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/dropdown/dropdown-item/dropdown-item.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vm-ac-dropdown-item',
+  templateUrl: './dropdown-item.component.html',
+  styleUrls: ['./dropdown-item.component.css']
+})
+export class DropdownItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

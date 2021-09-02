ee215f04d5e3b9bcb10706685f057625e09a880b
Vasilis Milionis <v_milionis@yahoo.com>
2021-09-02 14:42:46 +0300

    Adds On right click revert dropdown button text functionality

--- src/app/dropdown/dropdown.component.ts
+++ src/app/dropdown/dropdown.component.ts
@@ -9,6 +9,7 @@ import { DropdownMessagePassingService } from './dropdown-message-passing.servic
 export class DropdownComponent implements OnInit {
   @Input() name!: string;
   @Input() dropdown_id!: number;
+  private default_name!: string;
 
   constructor(private messageService: DropdownMessagePassingService) { }
 
@@ -16,5 +17,18 @@ export class DropdownComponent implements OnInit {
     this.messageService.initServiceInstance(this.dropdown_id, this.name).subscribe(
       text => this.name = text
     );
+    this.default_name = this.name;
+  }
+
+  // If the user right-clicks on the dropdown button
+  // it should reset to it's initial state
+  public onRClick(event: MouseEvent) {
+    event.preventDefault();
+    let x = event.target as HTMLButtonElement;
+    if (x.tagName.toLowerCase() !== "button" || event.button !== 2) {
+      return;
+    }
+    // Reset the button text
+    this.name = this.default_name;
   }
 }

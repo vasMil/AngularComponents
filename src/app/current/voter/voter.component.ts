2e3c061cd768c5dc63ad5b5511adbe5978e2237b
Vasilis Milionis <v_milionis@yahoo.com>
2021-09-12 16:52:52 +0300

    Initial voter module/component commit

---
+++ src/app/current/voter/voter.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'vm-voter',
+  templateUrl: './voter.component.html',
+  styleUrls: ['./voter.component.css']
+})
+export class VoterComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/current/voter/voter.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { VoterComponent } from './voter.component';
+
+
+
+@NgModule({
+  declarations: [
+    VoterComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class VoterModule { }
